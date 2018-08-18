b9a2b4862be78af6ac447c5d809732ad54a0e9c3
Miguel Martinez <slverwolf@gmail.com>
2018-08-18 13:52:43 -0500

    Add toggle for heads up notifications

--- src/com/skydragon/settings/HeadsUpSettings.java
+++ src/com/skydragon/settings/HeadsUpSettings.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.skydragon.settings.headsup;
+package com.skydragon.settings;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
