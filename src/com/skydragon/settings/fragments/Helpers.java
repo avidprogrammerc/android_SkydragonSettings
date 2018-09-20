Project-Xtended/packages_apps_Xtensions

Xtensions: Omni Doze settings & TimeInState [2/2]

src/com/msm/xtended/fragments/Helpers.java
@@ -1,0 +1,94 @@
+/*
+ * Performance Control - An Android CPU Control application Copyright (C) 2012
+ * Jared Rummler Copyright (C) 2012 James Roberts
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.msm.xtended.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Helpers {
+    public static final String TIME_IN_STATE_PATH = "/sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state";
+    public static final String NUM_OF_CPUS_PATH = "/sys/devices/system/cpu/present";
+    public static final String TIME_IN_STATE_OVERALL_PATH = "/sys/devices/system/cpu/cpufreq/overall_stats/overall_time_in_state";
+
+    /**
+     * Read one line from file
+     *
+     * @param fname
+     * @return line
+     */
+    public static String readOneLine(String fname) {
+        String line = null;
+        if (new File(fname).exists()) {
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader(fname), 512);
+                try {
+                    line = br.readLine();
+                } finally {
+                    br.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return line;
+    }
+
+    /**
+     * Get total number of cpus
+     *
+     * @return total number of cpus
+     */
+    public static int getNumOfCpus() {
+        int numOfCpu = 1;
+        String numOfCpus = Helpers.readOneLine(NUM_OF_CPUS_PATH);
+        if (numOfCpus == null) {
+            return numOfCpu;
+        }
+        String[] cpuCount = numOfCpus.split("-");
+        if (cpuCount.length > 1) {
+            try {
+                int cpuStart = Integer.parseInt(cpuCount[0]);
+                int cpuEnd = Integer.parseInt(cpuCount[1]);
+
+                numOfCpu = cpuEnd - cpuStart + 1;
+
+                if (numOfCpu < 0)
+                    numOfCpu = 1;
+            } catch (NumberFormatException ex) {
+                numOfCpu = 1;
+            }
+        }
+        return numOfCpu;
+    }
+
+    public static boolean hasOverallStats() {
+        return new File(TIME_IN_STATE_OVERALL_PATH).exists();
+    }
+}
+

src/com/msm/xtended/fragments/KeyValueListParser.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.msm.xtended.fragments;
+
+import android.text.TextUtils;
+import android.util.ArrayMap;
+
+/**
+ * Parses a list of key=value pairs, separated by some delimiter, and puts the results in
+ * an internal Map. Values can be then queried by key, or if not found, a default value
+ * can be used.
+ * @hide
+ */
+public class KeyValueListParser {
+    private final ArrayMap<String, String> mValues = new ArrayMap<>();
+    private final TextUtils.StringSplitter mSplitter;
+
+    /**
+     * Constructs a new KeyValueListParser. This can be reused for different strings
+     * by calling {@link #setString(String)}.
+     * @param delim The delimiter that separates key=value pairs.
+     */
+    public KeyValueListParser(char delim) {
+        mSplitter = new TextUtils.SimpleStringSplitter(delim);
+    }
+
+    /**
+     * Resets the parser with a new string to parse. The string is expected to be in the following
+     * format:
+     * <pre>key1=value,key2=value,key3=value</pre>
+     *
+     * where the delimiter is a comma.
+     *
+     * @param str the string to parse.
+     * @throws IllegalArgumentException if the string is malformed.
+     */
+    public void setString(String str) throws IllegalArgumentException {
+        mValues.clear();
+        if (str != null) {
+            mSplitter.setString(str);
+            for (String pair : mSplitter) {
+                int sep = pair.indexOf('=');
+                if (sep < 0) {
+                    mValues.clear();
+                    throw new IllegalArgumentException(
+                            "'" + pair + "' in '" + str + "' is not a valid key-value pair");
+                }
+                mValues.put(pair.substring(0, sep).trim(), pair.substring(sep + 1).trim());
+            }
+        }
+    }
+
+    /**
+     * Get the value for key as a long.
+     * @param key The key to lookup.
+     * @param def The value to return if the key was not found, or the value was not a long.
+     * @return the long value associated with the key.
+     */
+    public long getLong(String key, long def) {
+        String value = mValues.get(key);
+        if (value != null) {
+            try {
+                return Long.parseLong(value);
+            } catch (NumberFormatException e) {
+                // fallthrough
+            }
+        }
+        return def;
+    }
+
+    /**
+     * Get the value for key as a float.
+     * @param key The key to lookup.
+     * @param def The value to return if the key was not found, or the value was not a float.
+     * @return the float value associated with the key.
+     */
+    public float getFloat(String key, float def) {
+        String value = mValues.get(key);
+        if (value != null) {
+            try {
+                return Float.parseFloat(value);
+            } catch (NumberFormatException e) {
+                // fallthrough
+            }
+        }
+        return def;
+    }
+
+    /**
+     * Get the value for key as a string.
+     * @param key The key to lookup.
+     * @param def The value to return if the key was not found.
+     * @return the string value associated with the key.
+     */
+    public String getString(String key, String def) {
+        String value = mValues.get(key);
+        if (value != null) {
+            return value;
+        }
+        return def;
+    }
+}
+

src/com/msm/xtended/fragments/Profiles.java
@@ -1,0 +1,116 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+package com.msm.xtended.fragments;
+
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profiles {
+
+    public static class Profile implements Comparable<Profile> {
+
+        private String mName;
+        private String mSettings;
+
+        public Profile(String name, String settings){
+            mName = name;
+            mSettings = settings;
+        }
+
+        public String getName() {
+            return mName;
+        }
+
+        public String getSettings() {
+            return mSettings;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || ! (o instanceof Profile)) {
+                return false;
+            }
+            return mName.equals(((Profile)o).getName());
+        }
+
+        @Override
+        public int compareTo(Profile profile) {
+            return mName.compareTo(profile.getName());
+        }
+    }
+
+    private static final String PROFILE_PREFS = "profiles";
+
+    private static List<Profile> mUserProfileList = new ArrayList<>();
+    private static List<Profile> mSysteProfileList = new ArrayList<>();
+
+    public static void initProfiles() {
+        mSysteProfileList.clear();
+	mSysteProfileList.add(new Profile("aggressive", "light_after_inactive_to=30000,light_pre_idle_to=30000,light_idle_to=30000,light_idle_factor=2.0,light_max_idle_to=60000,light_idle_maintenance_min_budget=30000,light_idle_maintenance_max_budget=60000,min_light_maintenance_time=5000,min_deep_maintenance_time=10000,inactive_to=60000,sensing_to=0,locating_to=10000,location_accuracy=20.0,motion_inactive_to=60000,idle_after_inactive_to=0,idle_pending_to=30000,max_idle_pending_to=60000,idle_pending_factor=2.0,idle_to=3600000,max_idle_to=21600000,idle_factor=2.0,min_time_to_alarm=3600000,max_temp_app_whitelist_duration=20000,mms_temp_app_whitelist_duration=20000,sms_temp_app_whitelist_duration=20000,notification_whitelist_duration=20000"));
+	mSysteProfileList.add(new Profile("xtendedprofile", "light_after_inactive_to=60000, light_pre_idle_to=60000, light_idle_to=70000, light_idle_factor=2.0, light_max_idle_to=90000, light_idle_maintenance_min_budget=60000, light_idle_maintenance_max_budget=100000, min_light_maintenance_time=5000, min_deep_maintenance_time=30000, inactive_to=100000, sensing_to=0, locating_to=10000, location_accuracy=20.0, motion_inactive_to=30000, idle_after_inactive_to=0, idle_pending_to=60000, max_idle_pending_to=100000, idle_pending_factor=2.0, idle_to=180000, max_idle_to=360000, idle_factor=2.0, min_time_to_alarm=180000, max_temp_app_whitelist_duration=30000, mms_temp_app_whitelist_duration=30000, sms_temp_app_whitelist_duration=20000"));
+    }
+
+    public static List<Profile> getUserProfileList() {
+        return mUserProfileList;
+    }
+
+    public static List<Profile> getSystemProfileList() {
+        return mSysteProfileList;
+    }
+
+    public static void addProfile(Profile profile) {
+        if (mUserProfileList.contains(profile)) {
+            mUserProfileList.remove(profile);
+        }
+        mUserProfileList.add(profile);
+    }
+
+    public static void removeProfile(Profile profile) {
+        mUserProfileList.remove(profile);
+    }
+
+    public static void loadUserProfiles(SharedPreferences sharedPref) {
+        String userProfile = sharedPref.getString(PROFILE_PREFS , null);
+        mUserProfileList.clear();
+        if (!TextUtils.isEmpty(userProfile)) {
+            String[] profiles = userProfile.split(";");
+            for (String profile : profiles) {
+                String[] profileParts = profile.split(":");
+                Profile p = new Profile(profileParts[0], profileParts[1]);
+                mUserProfileList.add(p);
+            }
+        }
+    }
+
+    public static void saveUserProfiles(SharedPreferences sharedPref){
+        if (mUserProfileList.size() != 0) {
+            StringBuffer sb = new StringBuffer();
+            for (Profile p : mUserProfileList) {
+                sb.append(p.getName() + ":" + p.getSettings() + ";");
+            }
+            sharedPref.edit().putString(PROFILE_PREFS, sb.toString()).commit();
+        } else {
+            sharedPref.edit().remove(PROFILE_PREFS).commit();
+        }
+    }
+}
+
+

avidprogrammerc/android_SkydragonSettings

src/com/skydragon/settings/fragments/Helpers.java
@@ -1,0 +1,94 @@
+/*
+ * Performance Control - An Android CPU Control application Copyright (C) 2012
+ * Jared Rummler Copyright (C) 2012 James Roberts
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.skydragon.settings.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Helpers {
+    public static final String TIME_IN_STATE_PATH = "/sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state";
+    public static final String NUM_OF_CPUS_PATH = "/sys/devices/system/cpu/present";
+    public static final String TIME_IN_STATE_OVERALL_PATH = "/sys/devices/system/cpu/cpufreq/overall_stats/overall_time_in_state";
+
+    /**
+     * Read one line from file
+     *
+     * @param fname
+     * @return line
+     */
+    public static String readOneLine(String fname) {
+        String line = null;
+        if (new File(fname).exists()) {
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader(fname), 512);
+                try {
+                    line = br.readLine();
+                } finally {
+                    br.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return line;
+    }
+
+    /**
+     * Get total number of cpus
+     *
+     * @return total number of cpus
+     */
+    public static int getNumOfCpus() {
+        int numOfCpu = 1;
+        String numOfCpus = Helpers.readOneLine(NUM_OF_CPUS_PATH);
+        if (numOfCpus == null) {
+            return numOfCpu;
+        }
+        String[] cpuCount = numOfCpus.split("-");
+        if (cpuCount.length > 1) {
+            try {
+                int cpuStart = Integer.parseInt(cpuCount[0]);
+                int cpuEnd = Integer.parseInt(cpuCount[1]);
+
+                numOfCpu = cpuEnd - cpuStart + 1;
+
+                if (numOfCpu < 0)
+                    numOfCpu = 1;
+            } catch (NumberFormatException ex) {
+                numOfCpu = 1;
+            }
+        }
+        return numOfCpu;
+    }
+
+    public static boolean hasOverallStats() {
+        return new File(TIME_IN_STATE_OVERALL_PATH).exists();
+    }
+}
+

src/com/skydragon/settings/fragments/KeyValueListParser.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydragon.settings.fragments;
+
+import android.text.TextUtils;
+import android.util.ArrayMap;
+
+/**
+ * Parses a list of key=value pairs, separated by some delimiter, and puts the results in
+ * an internal Map. Values can be then queried by key, or if not found, a default value
+ * can be used.
+ * @hide
+ */
+public class KeyValueListParser {
+    private final ArrayMap<String, String> mValues = new ArrayMap<>();
+    private final TextUtils.StringSplitter mSplitter;
+
+    /**
+     * Constructs a new KeyValueListParser. This can be reused for different strings
+     * by calling {@link #setString(String)}.
+     * @param delim The delimiter that separates key=value pairs.
+     */
+    public KeyValueListParser(char delim) {
+        mSplitter = new TextUtils.SimpleStringSplitter(delim);
+    }
+
+    /**
+     * Resets the parser with a new string to parse. The string is expected to be in the following
+     * format:
+     * <pre>key1=value,key2=value,key3=value</pre>
+     *
+     * where the delimiter is a comma.
+     *
+     * @param str the string to parse.
+     * @throws IllegalArgumentException if the string is malformed.
+     */
+    public void setString(String str) throws IllegalArgumentException {
+        mValues.clear();
+        if (str != null) {
+            mSplitter.setString(str);
+            for (String pair : mSplitter) {
+                int sep = pair.indexOf('=');
+                if (sep < 0) {
+                    mValues.clear();
+                    throw new IllegalArgumentException(
+                            "'" + pair + "' in '" + str + "' is not a valid key-value pair");
+                }
+                mValues.put(pair.substring(0, sep).trim(), pair.substring(sep + 1).trim());
+            }
+        }
+    }
+
+    /**
+     * Get the value for key as a long.
+     * @param key The key to lookup.
+     * @param def The value to return if the key was not found, or the value was not a long.
+     * @return the long value associated with the key.
+     */
+    public long getLong(String key, long def) {
+        String value = mValues.get(key);
+        if (value != null) {
+            try {
+                return Long.parseLong(value);
+            } catch (NumberFormatException e) {
+                // fallthrough
+            }
+        }
+        return def;
+    }
+
+    /**
+     * Get the value for key as a float.
+     * @param key The key to lookup.
+     * @param def The value to return if the key was not found, or the value was not a float.
+     * @return the float value associated with the key.
+     */
+    public float getFloat(String key, float def) {
+        String value = mValues.get(key);
+        if (value != null) {
+            try {
+                return Float.parseFloat(value);
+            } catch (NumberFormatException e) {
+                // fallthrough
+            }
+        }
+        return def;
+    }
+
+    /**
+     * Get the value for key as a string.
+     * @param key The key to lookup.
+     * @param def The value to return if the key was not found.
+     * @return the string value associated with the key.
+     */
+    public String getString(String key, String def) {
+        String value = mValues.get(key);
+        if (value != null) {
+            return value;
+        }
+        return def;
+    }
+}
+

src/com/skydragon/settings/fragments/Profiles.java
@@ -1,0 +1,116 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+package com.skydragon.settings.fragments;
+
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profiles {
+
+    public static class Profile implements Comparable<Profile> {
+
+        private String mName;
+        private String mSettings;
+
+        public Profile(String name, String settings){
+            mName = name;
+            mSettings = settings;
+        }
+
+        public String getName() {
+            return mName;
+        }
+
+        public String getSettings() {
+            return mSettings;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || ! (o instanceof Profile)) {
+                return false;
+            }
+            return mName.equals(((Profile)o).getName());
+        }
+
+        @Override
+        public int compareTo(Profile profile) {
+            return mName.compareTo(profile.getName());
+        }
+    }
+
+    private static final String PROFILE_PREFS = "profiles";
+
+    private static List<Profile> mUserProfileList = new ArrayList<>();
+    private static List<Profile> mSysteProfileList = new ArrayList<>();
+
+    public static void initProfiles() {
+        mSysteProfileList.clear();
+	mSysteProfileList.add(new Profile("aggressive", "light_after_inactive_to=30000,light_pre_idle_to=30000,light_idle_to=30000,light_idle_factor=2.0,light_max_idle_to=60000,light_idle_maintenance_min_budget=30000,light_idle_maintenance_max_budget=60000,min_light_maintenance_time=5000,min_deep_maintenance_time=10000,inactive_to=60000,sensing_to=0,locating_to=10000,location_accuracy=20.0,motion_inactive_to=60000,idle_after_inactive_to=0,idle_pending_to=30000,max_idle_pending_to=60000,idle_pending_factor=2.0,idle_to=3600000,max_idle_to=21600000,idle_factor=2.0,min_time_to_alarm=3600000,max_temp_app_whitelist_duration=20000,mms_temp_app_whitelist_duration=20000,sms_temp_app_whitelist_duration=20000,notification_whitelist_duration=20000"));
+	mSysteProfileList.add(new Profile("skydragon profile", "light_after_inactive_to=60000, light_pre_idle_to=60000, light_idle_to=70000, light_idle_factor=2.0, light_max_idle_to=90000, light_idle_maintenance_min_budget=60000, light_idle_maintenance_max_budget=100000, min_light_maintenance_time=5000, min_deep_maintenance_time=30000, inactive_to=100000, sensing_to=0, locating_to=10000, location_accuracy=20.0, motion_inactive_to=30000, idle_after_inactive_to=0, idle_pending_to=60000, max_idle_pending_to=100000, idle_pending_factor=2.0, idle_to=180000, max_idle_to=360000, idle_factor=2.0, min_time_to_alarm=180000, max_temp_app_whitelist_duration=30000, mms_temp_app_whitelist_duration=30000, sms_temp_app_whitelist_duration=20000"));
+    }
+
+    public static List<Profile> getUserProfileList() {
+        return mUserProfileList;
+    }
+
+    public static List<Profile> getSystemProfileList() {
+        return mSysteProfileList;
+    }
+
+    public static void addProfile(Profile profile) {
+        if (mUserProfileList.contains(profile)) {
+            mUserProfileList.remove(profile);
+        }
+        mUserProfileList.add(profile);
+    }
+
+    public static void removeProfile(Profile profile) {
+        mUserProfileList.remove(profile);
+    }
+
+    public static void loadUserProfiles(SharedPreferences sharedPref) {
+        String userProfile = sharedPref.getString(PROFILE_PREFS , null);
+        mUserProfileList.clear();
+        if (!TextUtils.isEmpty(userProfile)) {
+            String[] profiles = userProfile.split(";");
+            for (String profile : profiles) {
+                String[] profileParts = profile.split(":");
+                Profile p = new Profile(profileParts[0], profileParts[1]);
+                mUserProfileList.add(p);
+            }
+        }
+    }
+
+    public static void saveUserProfiles(SharedPreferences sharedPref){
+        if (mUserProfileList.size() != 0) {
+            StringBuffer sb = new StringBuffer();
+            for (Profile p : mUserProfileList) {
+                sb.append(p.getName() + ":" + p.getSettings() + ";");
+            }
+            sharedPref.edit().putString(PROFILE_PREFS, sb.toString()).commit();
+        } else {
+            sharedPref.edit().remove(PROFILE_PREFS).commit();
+        }
+    }
+}
+
+