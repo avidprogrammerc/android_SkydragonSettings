8d23e26fc61b45693e84c9c40dbec2b49a2f6ae1
xyyx <xyyx@mail.ru>
2018-10-07 10:36:24 +0800

    SkyDragonSettings: Fix BatteryLight and Gesture path and name

--- src/com/skydragon/settings/fragments/BatteryLightSettings.java
+++ src/com/skydragon/settings/fragments/BatteryLightSettings.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.skydragon.settings;
+package com.skydragon.settings.fragments;
 
 import android.content.ContentResolver;
 import android.content.Context;
